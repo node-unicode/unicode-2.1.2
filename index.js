fba6f15058cfbed453d622b5c4bce7c86ad09052
Mathias Bynens <mathias@qiwi.be>
2014-05-28 15:29:00 +0200

    Tag the v0.1.4 release

---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports={'properties':['Any','Assigned','ASCII'],'categories':['Cc','C','Zs','Z','Po','P','Sc','S','Ps','Pe','Sm','Pd','Nd','N','Lu','L','LC','Sk','Pc','Ll','So','No','Lo','Lt','Cn','Lm','Mn','M','Me','Mc','Cf','Zl','Zp','Nl','Cs','Co'],'bidi':['ON','S','WS','ET','CS','ES','EN','L','R','AN','B']}
